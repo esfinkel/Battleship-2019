64889ac80308b33a911db6c4bed92642456c7053
Stefan Brechter <scb262@cornell.edu>
2019-10-24 13:29:25 -0400

    Commented out main.ml until import errors are fixed

--- main.ml
+++ main.ml
@@ -1,5 +1,5 @@
 (** [print_help] prints the list of valid commands. *)
-let print_help unit : unit = 
+(*let print_help unit : unit = 
   ANSITerminal.(print_string [blue] 
                   (String.concat "" 
                      [ "\n\nGame set-up commands:";
@@ -13,13 +13,21 @@ let print_help unit : unit =
                        "\nUse 'quit' to quit the game."
                      ])); ()
 
+  let display_board board =
 
+
+  let board_setup p1_board p2_board = 
+    display_board p1_board;
+    ANSITerminal.(print_string [blue]
+                    "\n\nPlayer 1 please set up your board.\n");
+    ()
+*)
 (** [main ()] prompts for the game to play, then starts it. *)
-let main () =
+(*let main () =
   ANSITerminal.(print_string [blue]
                   "\n\nWelcome to Battleship!\n");
   print_help ();
-  board_setup ()
-
+  board_setup (Board.init_board ()) (Board.init_board ())
+*)
 (* Execute the game engine. *)
-let () = main ()
+(*let () = main () *)
