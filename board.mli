13dc4b7bbd07604aa746e35edf9231e30d28a9b5
Elisabeth Finkel <esf76@cornell.edu>
2019-10-24 12:52:04 -0400

    started commenting

--- board.mli
+++ board.mli
@@ -1,7 +1,25 @@
+(* move these to the ml file *)
+(* AF : *)
+(* RI : *)
+
+(** The abstract type of values representing one player's battleship board. *)
 type t 
 
+type ship = {
+  name : Command.ship_name;
+  size : int
+}
+
+type spot =  Water | ShotWater | Ship of ship | HitShip of ship
+(* variant type opponent_spot (hidden | hit_ship | shot_missed) *)
+(* variant type Spot (MySpot of my_spot | OpponentSpot of opponent_spot) *)
+
+
+(** [init_board ()] is a new battleship board, with all cells initialized
+    to Water *)
 val init_board : unit -> t 
 
+(** [place (n, l1, l2) s] is unit. *)
 val place : Command.ship_name * Command.location * Command.location -> t -> unit 
 
 val remove : Command.ship_name -> t -> unit 
