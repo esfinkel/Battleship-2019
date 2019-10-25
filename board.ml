5c35cc248a23a2bcf3724ec873b115cdbffbbe4a
Elisabeth Finkel <esf76@cornell.edu>
2019-10-24 22:22:56 -0400

    a few helpers for ship placement

--- board.ml
+++ board.ml
@@ -45,8 +45,47 @@ let init_board () = Water
                     |> Array.make board_size
                     |> Array.make board_size
 
+(** [on_board loc] is true iff [loc] refers to a valid location on board
+    [b]. *)
+let on_board (loc : Command.location) (b : t) =
+  let size = Array.length b in
+  match row_col loc with 
+  | (r, c) when (0 <= r && r < size) && (0 <= c && c < size) -> true
+  | _ -> false
+
+(** [aligned loc1 loc2] is true iff [loc1] and [loc2] are in the
+    same row or column. *)
+let aligned loc1 loc2 =
+  match row_col loc1, row_col loc2 with
+  | (r1, c1), (r2, c2) when r1=r2 || c1=c2 -> true
+  | _ -> false
+
+(** [right_length loc1 loc2 s] is true iff the inclusive distance
+    between loc1 and loc2 equals the size of [s]. *)
+let right_length loc1 loc2 s =
+  let (r1, c1), (r2, c2) = row_col loc1, row_col loc2 in
+  if r1 = r2 then (c1 - c2 + 1 = s.size) || (c2 - c1 + 1 = s.size)
+  else if c1 = c2 then (r1 - r2 + 1 = s.size) || (r2 - r1 + 1 = s.size)
+  else false
+
+(** [duplicate_ship s b] is true iff [s] is already present on [b]. *)
+let duplicate_ship s b = 
+  failwith "unimplemented"
+
+(** [overlapping_ship s b] is true iff [s] would overlap with a ship
+    already present on [b]. *)
+let overlapping_ship s b =
+  failwith "unimplemented"
+
+(* this should call the above functions *)
 let place _ b =
   failwith "unimplemented"
+(* - OffBoard if [l1] or [l2] is off the game board *)
+(* - Misaligned if [l1] and [l2] are not in the same row or column *)
+(* - WrongLength if [l1] and [l2] are the wrong distance apart *)
+(* - DuplicateShip if the ship has already been placed *)
+(* - OverlappingShips if the ship would overlap with a ship already
+    present in [b]. *)
 
 let remove _ b = 
   failwith "unimplemented"
