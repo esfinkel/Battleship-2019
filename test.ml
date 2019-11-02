904a7cb646de25de465c3a39a311427b5725ee52
Elisabeth Finkel <esf76@cornell.edu>
2019-11-02 15:45:03 -0400

    finished pt1 tests maybe?

--- test.ml
+++ test.ml
@@ -97,53 +97,62 @@ let command_tests = [
 
 (* Some of these tests make Board.t break the "all ships have been placed"
    invariant. This couldn't happen during gameplay. *)
-let b1 = Board.init_board "fake name"
-let b2 = Board.init_board "fake name 2"
-let () = Board.place "battleship" "b2" "e2" b1
-let _ = Board.shoot "a3" b2
+let bd1 = Board.init_board "fake name"
+let () = Board.place "battleship" "b2" "e2" bd1
+
+let bd2 = Board.init_board "fake name 2"
+let _ = Board.shoot "a3" bd2
+
+let bd_lose = Board.init_board "this player will lose"
+let () = Board.(
+    place "battleship" "a1" "a4" bd_lose;
+    place "cruiser" "b1" "b2" bd_lose;
+    place "carrier" "c1" "c5" bd_lose;
+    place "destroyer" "d1" "d3" bd_lose;
+    place "submarine" "e1" "e3" bd_lose;
+  )
+let shoot_locs b =
+  List.fold_left (fun () loc -> Board.shoot loc b |> ignore) ()
+let () = shoot_locs bd_lose [
+    "a1"; "a2"; "a3"; "a4";
+    "b1"; "b2";
+    "c1"; "c2"; "c3"; "c4"; "c5";
+    "d1"; "d2"; "d3";
+    "e1"; "e2"; "e3"
+  ] 
+
+let bd_full = Board.init_board "all ships placed"
+let () = Board.place "default" "" "" bd_full
 
 let board_tests = [
-  make_equal_test "board player name" Board.player_name b1 "fake name";
+  make_equal_test "board player name" Board.player_name bd1 "fake name";
 
   make_board_op_exn_test "OverlappingShips"
-    (Board.place "destroyer" "b1" "b3") b1 Board.OverlappingShips;
+    (Board.place "destroyer" "b1" "b3") bd1 Board.OverlappingShips;
   make_board_op_exn_test "InvalidLoc"
-    (Board.place "destroyer" "b9" "b12") b1 Board.OffBoard;
+    (Board.place "destroyer" "b9" "b12") bd1 Board.OffBoard;
   make_board_op_exn_test "InvalidShipName"
-    (Board.place "destroyor" "b9" "b12") b1 Board.InvalidShipName;
+    (Board.place "destroyor" "b9" "b12") bd1 Board.InvalidShipName;
   make_board_op_exn_test "WrongLength"
-    (Board.place "destroyer" "b2" "b9") b1 Board.WrongLength;
+    (Board.place "destroyer" "b2" "b9") bd1 Board.WrongLength;
   make_board_op_exn_test "Misaligned"
-    (Board.place "destroyer" "b2" "c7") b1 Board.Misaligned;
+    (Board.place "destroyer" "b2" "c7") bd1 Board.Misaligned;
 
-  make_no_exn_raised_test "place indices are reversed"
-    (Board.place "cruiser" "h5" "h4") b1;
+  make_no_exn_raised_test "insertion indices are reversed"
+    (Board.place "cruiser" "h5" "h4") bd2;
 
-  make_equal_test "incomplete" Board.complete b1 false;
+  make_equal_test "incomplete" Board.complete bd1 false;
+  make_equal_test "complete" Board.complete bd_full true;
 
-
-  (* insert the other ships *)
-
-
-  (* make_equal_test "complete" Board.complete b1 true; *)
-
-  (* we will test shoot by gameplay. unless someone wants to  *)
   make_no_exn_raised_test "can shoot without error"
-    (Board.shoot "a3") b1; 
-
+    (Board.shoot "a3") bd1; 
   make_board_op_exn_test "shoot shot location" (Board.shoot "a3")
-    b2 Board.DuplicateShot;
+    bd2 Board.DuplicateShot;
   make_board_op_exn_test "invalid shot location"
-    (Board.shoot "a99") b1 Board.InvalidLoc;
+    (Board.shoot "a99") bd1 Board.InvalidLoc;
 
-  (* shoot remaining ships *)
-
-  (* make_equal_test "b1 has lost" Board.status b1
-     "All of your ships have been destroyed."; *)
-
-  (* let's just test string_self and string_other by inspection, unless
-     someone really wants to make the grid *)
-
+  make_equal_test "bd_lose has lost" Board.did_lose bd_lose true;
+  make_equal_test "bd2 has not lost" Board.did_lose bd2 false;
 
 ]
 
