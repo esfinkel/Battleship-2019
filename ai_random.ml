07365f44fa3534b8cd52f5989d17d28a0ca67d9b
Elisabeth Finkel <esf76@cornell.edu>
2019-11-04 15:11:09 -0500

    ai random

--- ai_random.ml
+++ ai_random.ml
@@ -4,13 +4,19 @@ type t = {
   name: string;
 }
 
-let init () = {
-  board = Board.init_board "";
-  name = ""
-}
+let seed_random () = Unix.time () |> int_of_float |> Random.init
+
+let init () =
+  let () = seed_random () in
+  {
+    board = Board.init_board "";
+    name = ""
+  }
 
 let get_board c = c.board
 
 let place_all_ships c = Board.place "default" "" "" c.board
 
-let shoot_ship () = ()
+
+let rec shoot_ship b = try Board.shoot "a1" b with
+  | _ -> shoot_ship b
