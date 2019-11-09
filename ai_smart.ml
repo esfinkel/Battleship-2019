796e2dd63513b7d81d0a8d77e39c996fdbed29e8
Elisabeth Finkel <esf76@cornell.edu>
2019-11-09 16:05:27 -0500

    started shoot

--- ai_smart.ml
+++ ai_smart.ml
@@ -1,8 +1,11 @@
+type coor_type = int * int
 
 type t = {
   board: Board.t;
-  mutable history: (Command.location list)
+  mutable hit_history: (coor_type*bool) list (* bool represents
+                                                whether ship is alive *)
 }
+
 
 let seed_random () = Unix.time () |> int_of_float |> Random.init
 
@@ -10,7 +13,7 @@ let init () =
   let () = seed_random () in
   {
     board = Board.init_board "💻";
-    history = [];
+    hit_history = [];
   }
 
 let get_board c = c.board
@@ -19,13 +22,18 @@ let get_board c = c.board
 
 (* ship placement *)
 
-let ordered (pos1, pos2) = if pos1 < pos2 then pos1, pos2 else pos2, pos1
+(** [ordered (pos1, pos2)] is [(pos_a, pos_b)], where [pos_a] is the
+    lesser of the two and [pos_b] the greater. *)
+let ordered ((pos1, pos2) : coor_type * coor_type)
+  = if pos1 < pos2 then pos1, pos2 else pos2, pos1
 
 let board_size = 10
 
 let on_board a = 0 <= a && a < board_size
 let coor_on_board (a, b) = on_board a && on_board b
 
+(** [all_cells_between c1 c2] is all the grid cells (inclusive) between [c1]
+    and [c2]. *)
 let rec all_cells_between c1 c2 = 
   let (x_1, y_1), (x_2, y_2) = ordered (c1, c2) in
   match (x_1 < x_2, y_1 < y_2) with
@@ -34,6 +42,8 @@ let rec all_cells_between c1 c2 =
   | false, false -> [(x_1, y_1)]
   | true, true -> failwith "something has gone very wrong"
 
+(** [random_placement_coors size] is a random pair of coordinates that span
+    [size] grid cells. *)
 let random_placement_coors size =
   let c1_x = (Random.int board_size) in 
   let c1_y = (Random.int board_size) in 
@@ -45,48 +55,62 @@ let random_placement_coors size =
   | 2 -> c1, (c1_x, c1_y-size+1) (* down *)
   | _ -> c1, (c1_x-size+1, c1_y) (* left *)
 
+(** [no_overlap ship_cells off_limits] is true iff there is no overlap
+    between [ship_cells] and [off_limits]. *)
 let no_overlap ship_cells off_limits =
   List.fold_left
     (fun sofar coor -> sofar && (List.mem coor off_limits |> not))
     true ship_cells
 
+(** [adjacent_cells coor] is a list of the coordinates that are horizontally
+    or vertically adjacent to [coor]. *)
+let adjacent_cells (x, y) = (x+1,y)::(x-1,y)::(x,y-1)::(x,y+1)::[]
+
+(** [new_off_limits ship_cells off_limits] is [off_limits] with the
+    addition of the cells that are adjacent to any cell in [ship_cells]. *)
 let new_off_limits ship_cells off_limits =
-  List.fold_left (fun sofar (x,y) ->
-      (x,y)::(x+1,y)::(x-1,y)::(x,y-1)::(x,y+1)::sofar
+  List.fold_left (fun sofar coor ->
+      coor::(adjacent_cells coor) @ sofar
     ) off_limits ship_cells |> List.sort_uniq Stdlib.compare
 
+(** [place b sh c1 c2] is [()]; the ship named [sh] has been placed
+    on [b] with its ends on [c1] and [c2]. *)
 let place b sh c1 c2 = Board.place_m_r sh c1 c2 b
 
-(* Space the ships out so that they do not touch *)
-let rec place_ship_smartly (c:t) b name size off_limits =
+(** - [place_ship_smartly b name size off_limits] is a new list [res].
+    - The ship with [name] and [size] has been placed on [b] such that 
+      the ship does not overlap with the cells described by [off_limits].
+      In effect, the ship is not touching any existing ships.
+    - [res] if [off_limits] with the addition of the cells surrounding
+      the ship's location. *)
+let rec place_ship_smartly b name size off_limits =
   (* pick a random coor, and a diff random at appropriate distance *)
   let new_coors = random_placement_coors size in
   (* order them *)
   let c1, c2 = ordered new_coors in
   (* if anything off_board, recurse *)
   if not (coor_on_board c1 && coor_on_board c2)
-  then place_ship_smartly c b name size off_limits
+  then place_ship_smartly b name size off_limits
   else
     (* calculate coors for all ship cells *)
     let ship_cells = all_cells_between c1 c2 in
     (* if any overlap between that and off_limits, recurse *)
     if no_overlap ship_cells off_limits |> not
-    then place_ship_smartly c b name size off_limits
+    then place_ship_smartly b name size off_limits
     (* otherwise add to off_limits and return new off_limits *)
     else
       (place b name c1 c2;
        new_off_limits ship_cells off_limits)
 
-
+(** [place_all_ships c] is [()]. All ships have been placed on the
+    board belonging to [c], such that no ships are touching. *)
 let place_all_ships c = 
   let to_place = Board.setup_status_m_r c.board in
   List.fold_left
-    (fun off_limits (n, s) -> place_ship_smartly c c.board n s off_limits)
+    (fun off_limits (n, s) -> place_ship_smartly c.board n s off_limits)
     [] to_place
   |> ignore
 
-
-let rec shoot c op = ()
 
 (* **************************** *)
 
@@ -98,20 +122,79 @@ let rec shoot c op = ()
 (* Use parity to up your chances *)
 (* Move away when you have two misses in the same segment *)
 
+let random_coors () =
+  let x = (Random.int board_size) in 
+  let y = (Random.int board_size) in 
+  (x, y)
+
+
+
+
 
 (* when shooting with history, keep track of direction -
    otherwise the same as ai_normal *)
 
+let rec coors_adjacent_to_this
+    (coor : coor_type)
+    (coor_list : (coor_type*bool) list) =
+  let is_adjacent (c,b) = (List.mem c (adjacent_cells coor) && b) in
+  List.filter (is_adjacent) coor_list
+  |> List.map (fun (c2, b) -> (coor, c2))
+
+let rec shoot_random c b = 
+  try 
+    let coor = random_coors () in 
+    match Board.shoot_m_r (coor) b with
+    | true, is_dead -> c.hit_history <- (coor, is_dead)::c.hit_history; ""
+    | _ -> ""
+  with | _ -> shoot_random c b
+
+let rec find_adjacent_xs ls = function
+  | [] -> ls
+  | (c, is_alive)::t -> find_adjacent_xs (ls @ (coors_adjacent_to_this c t)) t
+
+let shoot_find_unknown_nearby b adjx = 
+
+  let ends ((x1, y1), (x2, y2)) = if x1 = x2
+    then (* horiz *) [(x1, y1-1); (x2, y2+1)]
+    else (* vert *) [(x1-1, y1); (x2+1, y2)] in
+  let is_unknown = Board.is_unshot b in
+
+  List.fold_left (fun ls coors -> ls @ (coors |> ordered |> ends)) [] adjx
+  |> List.filter coor_on_board
+  |> List.filter is_unknown
+
+let update_history c =
+  c.hit_history <- (
+    List.map (fun (coor, _) ->
+        (coor, Board.is_part_of_living_ship c.board coor)
+      ) c.hit_history
+  )
+
+let rec shoot_find_nearby c b = 
+  update_history c;
+  (* update all the is_dead values in this hit_history *)
+  let grid = Board.string_other b in
+  (* look for adjacent X's in history *)
+  let adjx = find_adjacent_xs [] c.hit_history in
+  shoot_find_unknown_nearby b adjx
+
+
+  |> ignore; ""
+(* if you find adjacent x's with ? on at least one end; shoot that ? *)
+
+
+(* otherwise, shoot a ? next to any X *)
+
+(* if you don't find one, shoot randomly *)
+
+(* finally, update all the is_dead values in this hit_history *)
+
+
+
+let rec shoot_ship c b = 
+  if c.hit_history = [] then
+    shoot_random c b else
+    shoot_find_nearby c b
 
 (* **************************** *)
-
-
-
-let random_coors () =
-  let yaxis = Char.chr ((Random.int 10) + 65) |> String.make 1 in
-  let xaxis = string_of_int ((Random.int 10) + 1) in
-  yaxis ^ xaxis
-
-
-let rec shoot_ship c b = try Board.shoot (random_coors ()) b with
-  | _ -> shoot_ship c b
