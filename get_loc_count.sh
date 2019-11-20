1585041b4b06102877d3349bc456036f2cd5dc58
Elisabeth Finkel <esf76@cornell.edu>
2019-11-20 12:17:35 -0500

    shows test code

--- get_loc_count.sh
+++ get_loc_count.sh
@@ -1,1 +1,4 @@
+printf "\n\nwithout test\n\n"
 scc ai_normal.ml ai_random.ml ai_smart.ml board.ml command.ml helpers.ml main.ml
+printf "\n\n\nwith test\n\n"
+scc ai_normal.ml ai_random.ml ai_smart.ml board.ml command.ml helpers.ml main.ml test.ml
