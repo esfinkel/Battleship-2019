22729293a2114b4adafae9f1b72063982d529e3c
Elisabeth Finkel <esf76@cornell.edu>
2019-11-30 17:26:44 -0500

    updated

--- get_loc_count.sh
+++ get_loc_count.sh
@@ -1,4 +1,4 @@
 printf "\n\nwithout test\n\n"
-scc ai_normal.ml ai_random.ml ai_smart.ml board.ml command.ml helpers.ml main.ml
+scc ai_normal.ml ai_random.ml ai_smart.ml board.ml command.ml helpers.ml main.ml custom_board_parser.ml
 printf "\n\n\nwith test\n\n"
-scc ai_normal.ml ai_random.ml ai_smart.ml board.ml command.ml helpers.ml main.ml test.ml
+scc ai_normal.ml ai_random.ml ai_smart.ml board.ml command.ml helpers.ml main.ml custom_board_parser.ml test.ml
